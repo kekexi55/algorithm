64f3edb251830a2358967e9d0aacf2c56f0ab959
zhengchunguang <zhengchunguang@babyfs.cn>
2019-06-11 15:20:32 +0800

    CountDownLatch测试

---
+++ src/main/java/CTLTest.java
@@ -0,0 +1,14 @@
+import java.util.concurrent.CountDownLatch;
+
+/***
+ *
+ * @author zhengchunguang
+ * @date 2019-06-11 15:10
+ *  countdownlatch测试
+ */
+public class CTLTest {
+    public static void main(String[] args) {
+        CountDownLatch countDownLatch = new CountDownLatch(3);
+
+    }
+}
