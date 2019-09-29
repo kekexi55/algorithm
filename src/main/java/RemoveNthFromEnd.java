821579a311b5d7b74d9ee7b6df77ad4884ad1955
zhengchunguang <zhengchunguang@babyfs.cn>
2019-09-29 13:48:14 +0800

    找到倒数多少的数据

--- src/main/java/RemoveNthFromEnd.java
+++ src/main/java/RemoveNthFromEnd.java
@@ -1,27 +1,43 @@
+import java.util.Objects;
+
 /***
  *
  * @author zhengchunguang
  * @date 2019-05-09 18:39
- * 删除链表倒数第N个值
+ * 找到
  * 快慢指针法
  */
 public class RemoveNthFromEnd {
-
-    public static ListNode removeNthFromEnd(ListNode head ,int n) throws Exception {
-        if(head == null){
+    public static ListNode find(ListNode head, int n){
+        if(Objects.isNull(head)){
             return null;
         }
-
         ListNode p = head;
         ListNode q = head;
-        for (int i = 0; i < n; i++) {
-            p =  p.next;
+        for (int i = 0; i < n - 1; i++) {
+            p = p.next;
         }
-        while(p.next !=null){
+        while(Objects.nonNull(p.next)){
             p = p.next;
             q = q.next;
         }
-        q.next = q.next.next;
-        return head;
+//        q = q.next;
+        return q;
     }
+
+    public static void main(String[] args) {
+        ListNode p1 = new ListNode(10);
+        ListNode p2 = new ListNode(30);
+        ListNode p3 = new ListNode(40);
+        ListNode p4 = new ListNode(5);
+        ListNode p5 = new ListNode(35);
+        p5.next = null;
+        p4.next = p5;
+        p3.next = p4;
+        p2.next = p3;
+        p1.next = p2;
+
+        System.out.println(find(p1, 3).value);
+    }
+
 }
