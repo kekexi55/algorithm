45af61dea17db79c113c8b69a3fe4892789f8dd2
zhengchunguang <zhengchunguang@babyfs.cn>
2019-10-08 10:00:01 +0800

    commit

---
+++ src/main/java/CoinChange.java
@@ -0,0 +1,16 @@
+/***
+ *
+ * @author zhengchunguang
+ * @date 2019-10-06 11:26
+ * 兑换硬币
+ */
+public class CoinChange {
+    /**
+     * 硬币的种类
+     */
+    public static int[]coins = new int[]{1,2,5};
+
+    public int num(int amount){
+        return 0;
+    }
+}
