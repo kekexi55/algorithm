b67683b80bfa7f77eab4af35d70e7954a3310924
zhengchunguang <zhengchunguang@babyfs.cn>
2019-10-09 11:00:00 +0800

    再写一遍插入排序

--- src/main/java/InsertSort.java
+++ src/main/java/InsertSort.java
@@ -5,28 +5,36 @@
  * 插入排序
  */
 public class InsertSort {
-    public static void insertSort(int[] array){
-        int length = array.length;
-        for (int i = 1; i < length; i++) {
-            int value = array[i];
-            int j = i - 1;
-            for(;j >= 0;j--){
-                if(array[j] > value){
-                    array[j+1] = array[j];
-                }else{
-                    break;
-                }
-            }
-            array[j+1]= value;
-        }
 
-    }
 
     public static void main(String[] args) {
         int[] array = new int[]{882,199,2828,199182,2882,2929};
         insertSort(array);
         for (int i : array) {
             System.out.println(i);
+        }
+    }
+
+    /**
+     * 插入排序
+     * @param array
+     */
+    private static void insertSort(int[] array) {
+        if(null == array || array.length == 0){
+            return;
+        }
+        int length = array.length;
+        for (int i = 1; i < length; i++) {
+            int j = i - 1;
+            int tmp = array[i];
+            for (; j >= 0; j--) {
+                if(array[j]>array[i]){
+                    array[j+1] = array[j];
+                }else{
+                    break;
+                }
+            }
+            array[j+1] = tmp;
         }
     }
 }
