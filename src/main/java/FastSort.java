2ef965211ce63256c0270913eeff0e441eb9a279
zhengchunguang <zhengchunguang@babyfs.cn>
2019-12-19 10:30:29 +0800

    提取字符串中的字母

--- src/main/java/FastSort.java
+++ src/main/java/FastSort.java
@@ -37,6 +37,10 @@ public class FastSort {
     }
 
     public static void main(String[] args) {
+
+        String test = "alal 你好 ll";
+        test = test.replaceAll("[^a-z^A-Z]", "");
+        System.out.println(test);
         int[] array = new int[]{12,92,9390,8828,32,873,727};
         System.out.println("before");
         for (int i : array) {
@@ -48,6 +52,4 @@ public class FastSort {
             System.out.println(i);
         }
     }
-
-
 }
